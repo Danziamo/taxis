c7aac0b182647457be8688720dad0ef2e5dd33f3
unknown <dan.takyrbashev@gmail.com>
2015-05-29 19:12:55 +0600

    Setting role as driver on login

--- app/src/main/java/taxi/city/citytaxidriver/LoginActivity.java
+++ app/src/main/java/taxi/city/citytaxidriver/LoginActivity.java
@@ -316,6 +316,7 @@ public class LoginActivity extends Activity{
                         if (Helper.isSuccess(cars) && cars.has("result") && cars.getJSONArray("result").length() > 0) hasCar = true;
                         JSONObject regObject = new JSONObject();
                         regObject.put("online_status", "online");
+                        regObject.put("role", "driver");
                         regObject.put("ios_token", JSONObject.NULL);
                         JSONObject updateObject = api.patchRequest(regObject, "users/" + id + "/");
                         res = true;
